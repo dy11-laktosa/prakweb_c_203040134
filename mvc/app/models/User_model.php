ac99bf9a3474be0a25e803c63752db90973f788c
dy11-laktosa <hady4.1994@gmail.com>
2022-10-10 22:10:27 +0700

    menyelesaikan video 7 mvc

---
+++ mvc/app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+  private $nama = 'Hady Ismanto Rachmat';
+
+  public function getUser()
+  {
+    return $this->nama;
+  }
+}
---
+++ mvc/app/views/Home/index.php
@@ -0,0 +1,13 @@
+<body class="container bg-dark">
+
+  <div class="p-5 mb-4 bg-light rounded-3 bg-light mt-3">
+    <div class="container text-dark">
+      <h1 class="display-5 fw-bold">Selamat Datang di Website Saya!</h1>
+      <p class="col-md-8 fs-4">Halo, nama saya <?= $data['nama'] ?></p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <button class="btn btn-primary btn-lg" type="button">Example button</button>
+    </div>
+  </div>
+
+</body>
